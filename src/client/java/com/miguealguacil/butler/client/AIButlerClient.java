874af95de539dfdf7a3bebd345b892e2286366f3
Miguel Ángel Benítez <miguealguacil@gmail.com>
2026-05-25 19:08:45 +0200

    Initial Fabric mod project

---
+++ src/client/java/com/miguealguacil/butler/client/AIButlerClient.java
@@ -0,0 +1,10 @@
+package com.miguealguacil.butler.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AIButlerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
