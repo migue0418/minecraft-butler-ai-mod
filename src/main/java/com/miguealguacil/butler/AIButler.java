28e8f17b852d34937c024aa310d3f9617ff44ebd
Miguel Ángel Benítez <miguealguacil@gmail.com>
2026-05-26 00:51:38 +0200

    feat: load ChestRegistry on server start

--- src/main/java/com/miguealguacil/butler/AIButler.java
+++ src/main/java/com/miguealguacil/butler/AIButler.java
@@ -1,9 +1,11 @@
 package com.miguealguacil.butler;
 
+import com.miguealguacil.butler.chest.ChestRegistry;
 import com.miguealguacil.butler.command.ButlerCommand;
 import com.miguealguacil.butler.entity.AlfredEntities;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +16,7 @@ public class AIButler implements ModInitializer {
     @Override
     public void onInitialize() {
         AlfredEntities.register();
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> ChestRegistry.load());
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
             ButlerCommand.register(dispatcher)
         );
